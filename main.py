b6f66d8f2cb0764e7ae15164d58a8912546c6101
Matheus Matos <37658869+matosmatheus7@users.noreply.github.com>
2021-08-24 21:38:26 -0300

    Transformation

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
+import sqlalchemy
+import pandas as pd 
 from sqlalchemy.orm import sessionmaker
 import requests
+import json
 from datetime import datetime
 import datetime
-
+import sqlite3
 
 DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
 USER = "matosmatheux"
-TOKEN = "BQB2xjzmGz2-exz96MMDPsAVnSZryMTP3_XmrvaFCOvKJ8zDmg_ag6gt2Eg5a7GHkuAfYMjsa9gz-NiINuoj4zUZhfFoaOTLIV2xNsKdWTnXFI3J3P1lTqrF1ZOxja6qbfwnkrke4_Uh620mD26vktZqOUU"
+TOKEN = "BQAZ3r4e4C1of_n9V243CYOFlosDvTP9pguC3Rpd4vb0VRjFJGc1hHDaoTRDoTlIhX4btgkqhbXq-tK5M0esMyXx-IGpAjZGGWCx7TG0Uls2jajEov5eLni5_s7tEpHmmZr-wxoP2U9HtkuYRYKYm_du3mg"
+
+def check_if_valid_data(df: pd.DataFrame) -> bool:
+    # Check if dataframe is empty
+    if df.empty:
+        print("No songs were found it.")
+        return False 
+
+    # Primary Key Check
+    if pd.Series(df['played_at']).is_unique:
+        pass
+    else:
+        raise Exception("Primary Key violated")
+
+    # Check for nulls
+    if df.isnull().values.any():
+        raise Exception("Null values found")
+
+    # Check that all timestamps are of yesterday's date
+    yesterday = datetime.datetime.now() - datetime.timedelta(days=1)
+    yesterday = yesterday.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    timestamps = df["timestamp"].tolist()
+    for timestamp in timestamps:
+        if datetime.datetime.strptime(timestamp, '%Y-%m-%d') != yesterday:
+            raise Exception("One of the returned songs does not have a yesterday's timestamp")
+
+    return True
 
 if __name__ == "__main__":
 
@@ -29,3 +59,30 @@ if __name__ == "__main__":
     data = r.json()
 
     print(data)
+    
+    song_names = []
+    artist_names = []
+    played_at_list = []
+    timestamps = []
+
+    # Extracting only the relevant bits of data from the json object      
+    for song in data["items"]:
+        song_names.append(song["track"]["name"])
+        artist_names.append(song["track"]["album"]["artists"][0]["name"])
+        played_at_list.append(song["played_at"])
+        timestamps.append(song["played_at"][0:10])
+        
+    # Prepare a dictionary in order to turn it into a pandas dataframe below       
+    song_dict = {
+        "song_name" : song_names,
+        "artist_name": artist_names,
+        "played_at" : played_at_list,
+        "timestamp" : timestamps
+    }
+
+    song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
+    
+    # Validate
+    if check_if_valid_data(song_df):
+        print("Data valid, proceed to Load stage")
+
