e3ac3d8c595c8c4ed62cf9b6c82a74aa95edc2c3
Matheus Matos <37658869+matosmatheus7@users.noreply.github.com>
2021-08-24 21:49:10 -0300

    Load stage completed

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from datetime import datetime
 import datetime
 import sqlite3
 
-DATABASE_LOCATION = "sqlite:///my_played_tracks.sqlite"
-USER = "matosmatheux"
-TOKEN = "BQAZ3r4e4C1of_n9V243CYOFlosDvTP9pguC3Rpd4vb0VRjFJGc1hHDaoTRDoTlIhX4btgkqhbXq-tK5M0esMyXx-IGpAjZGGWCx7TG0Uls2jajEov5eLni5_s7tEpHmmZr-wxoP2U9HtkuYRYKYm_du3mg"
+DATABASE_LOCATION = "sqlite:///played_tracks.sqlite"
+USER = "USER_NAME"
+TOKEN = "YOUR_TOKEN"
 
-def check_if_valid_data(df: pd.DataFrame) -> bool:
+def valid_data(df: pd.DataFrame) -> bool:
     # Check if dataframe is empty
     if df.empty:
         print("No songs were found it.")
@@ -83,6 +83,28 @@ if __name__ == "__main__":
     song_df = pd.DataFrame(song_dict, columns = ["song_name", "artist_name", "played_at", "timestamp"])
     
     # Validate
-    if check_if_valid_data(song_df):
+    if valid_data(song_df):
         print("Data valid, proceed to Load stage")
 
+    engine = sqlalchemy.create_engine(DATABASE_LOCATION)
+    conn = sqlite3.connect('played_tracks.sqlite')
+    cursor = conn.cursor()
+
+    sql_query = """
+    CREATE TABLE IF NOT EXISTS my_played_tracks(
+        song_name VARCHAR(200),
+        artist_name VARCHAR(200),
+        played_at VARCHAR(200),
+        timestamp VARCHAR(200),
+        CONSTRAINT primary_key_constraint PRIMARY KEY (played_at)
+    )
+    """
+
+    cursor.execute(sql_query)
+    try:
+        song_df.to_sql("my_played_tracks", engine, index=False, if_exists='append')
+    except:
+        print("Data already exists in the database. Finishing Execution")
+
+    conn.close()
+    print("Close database successfully. Music(s) were loaded successfully")
